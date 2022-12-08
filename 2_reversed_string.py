1b82d9e1295590b9a36d21cf33b50e79100053fe
Gatuk Chattanon <gatukgl@gmail.com>
2022-12-08 20:15:22 +0700

    Reverse a sentence with python loop

--- 2_reversed_string.py
+++ 2_reversed_string.py
@@ -1,4 +1,14 @@
 my_data = "I love my annimal very much"
+
+# Using a loop
+data_length = len(my_data)
+reversed_string = ""
+for i in range(data_length - 1, -1, -1):
+    reversed_string += my_data[i] 
+print(reversed_string)
+
+# Using Python function
+my_data = "I love my annimal very much"
 list_of_strings = list(my_data)
 list_of_strings.reverse()
 reversed_string = "".join(list_of_strings)
