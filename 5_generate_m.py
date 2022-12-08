1b8b998cd79fa9ebe8576b0530457da205f03879
Gatuk Chattanon <gatukgl@gmail.com>
2022-12-08 21:24:49 +0700

    calculate half of block for M

--- 5_generate_m.py
+++ 5_generate_m.py
@@ -1,11 +1,12 @@
 generated_m = ""
 total_block = 9
+half_block = int(9 / 2)
 
 for i in range(0, total_block):
     for j in range(0, total_block):
         if j == 0 or j == total_block - 1:
             generated_m += "*"
-        elif (j == i or j == total_block - i - 1) and i <= 3:
+        elif (j == i or j == total_block - i - 1) and i < half_block:
             generated_m += "*"
         else:
             generated_m += " "
